9fc9b58f054751c5889fe148dbe15b13e15c3291
Tsvetelina <tswetti37@gmail.com>
2022-01-12 21:40:17 +0200

    Added start menu function

--- Email/Email/Email.cpp
+++ Email/Email/Email.cpp
@@ -1,7 +1,48 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
+
+int StartMenu();
+
+int StartMenu()
+{
+	//fstream users;
+	//users.open("users.txt", fstream::in);
+
+	char command;
+
+	cout << "Type a command: ";
+	do
+	{
+		cin >> command;
+		if (command == 'L')
+		{
+			return 0;
+		}
+		else if (command == 'R')
+		{
+			return 0;
+		}
+		else if (command == 'Q')
+		{
+			//users.close();
+			return 1;
+		}
+		else 
+		{
+			cout << "Invalid command! Try again: ";
+		}
+	} while (true);
+}
 
 int main()
 {
+	if (StartMenu() == 1)
+	{
+		return 1;
+	}
 
+	return 0;
 }
